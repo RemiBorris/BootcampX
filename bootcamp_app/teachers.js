97f7eb87a8b245217f9bb10282eea94d258b4e8c
remiborris <remiborris@gmail.com>
2024-07-30 17:17:22 -0300

    change queries to not intake input data directly into sql to avoid sql injection attack

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,24 +1,27 @@
 const { Pool } = require("pg");
 
-const pool = new Pool ({
+const pool = new Pool({
   user: "development",
   password: "development",
   host: "localhost",
   database: "bootcampx"
 });
 
-pool
-  .query(
-    `
+const queryString = `
     SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
     FROM assistance_requests
     JOIN teachers ON teachers.id = teacher_id
     JOIN students ON students.id = student_id
     JOIN cohorts ON cohorts.id = cohort_id
-    WHERE cohorts.name = '${process.argv[2]}'
+    WHERE cohorts.name = $1
     ORDER BY teacher;
-    `
-  )
+    `;
+
+const cohortName = process.argv[2];
+const values = [cohortName];
+
+pool
+  .query(queryString, values)
   .then((res) => {
     res.rows.forEach((res) => {
       console.log(`${res.cohort}: ${res.teacher}`);
